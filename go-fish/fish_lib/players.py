b426fa481e1bed1a2cd3a044b9f888421c933ac6
Justin Smith <JTaySmith.users.noreply.github.com>
2018-01-23 05:47:17 -0600

    Changed some docs and functionality.

--- go-fish/fish_lib/players.py
+++ go-fish/fish_lib/players.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import choice
 
 
 class BasePlayer(object):
@@ -27,10 +27,32 @@ class DumbPlayer(BasePlayer):
     def __init__(self, hand):
         super(DumbPlayer, self).__init__(hand)
 
-    def ask_for_card(self):
-        return randint(0, len(self.hand) - 1)[0]
+    def ask_for_card(self, players: list):
+        """
+        This method is intended to be called when a player asks for a card.
+
+        There is no strategy here: throws out a random card to a random player.
+
+        Parameters:
+            players:
+                A list of players in the same game with him.
+
+        :return: A tuple with 2 elements, a face value and the player to request the card from.
+        """
+        return choice(self.hand)[0], choice(players)
 
     def confirm_ask(self, face):
+        """
+        This method is intended to be called when a player is asked for a card.
+
+        This method will hand over all of the cards as asked.
+
+        Parameters:
+            face:
+                The requested face value.
+
+        :return: All of the cards that have the same face value.
+        """
         given_cards = []
         for i in (card for card in self.hand if card[0] == face):
             self.hand.remove(i)
