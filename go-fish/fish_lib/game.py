2b720c0264574d28b4a305f649bf1074ac8f8008
Justin Smith <JTaySmith.users.noreply.github.com>
2018-01-23 06:44:32 -0600

    Added a single turn function to game.py

--- go-fish/fish_lib/game.py
+++ go-fish/fish_lib/game.py
@@ -1,8 +1,7 @@
 import itertools
-
 from random import choice
-from . import players
 
+from . import players
 
 RANKS = (None, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace')
 SUITS = ('Spades', 'Clubs', 'Hearts', 'Diamond')
@@ -16,6 +15,7 @@ class GoFish(object):
         if player_count > 8 or player_count < 2:
             raise ValueError('Player count is out of range! 2 <= player_count <= 8.')
 
+        self.active_player = 0
         self.players = []
         player_count = player_count
         valid_types = player_types
@@ -25,3 +25,18 @@ class GoFish(object):
             player_hand = list(BASE_DECK[card_count * i:card_count * (i + 1)])
             player = choice(valid_types)(player_hand)
             self.players.append(player)
+
+    def do_turn(self):
+        # We pass the players in case the Player is keeping tracking of that.
+        # requested face and requested player.
+        r_face, r_player = self.players[self.active_player].ask_for_card(self.players)
+
+        # Add the cards obtained straight into the player's hand.
+        won_cards = r_player.confirm_ask(r_face)
+        if won_cards:
+            self.players[self.active_player].hand.extend(won_cards)
+        else:
+            self.players[self.active_player].hand.append(self.deck.pop(0))
+
+        # Increment the player index safely.
+        self.active_player = (self.active_player + 1) % len(self.players)
--- go-fish/main.py
+++ go-fish/main.py
@@ -1,4 +1,5 @@
 from fish_lib import game
 
 if __name__ == '__main__':
-    game.GoFish()
+    x = game.GoFish()
+    print(x.players)
