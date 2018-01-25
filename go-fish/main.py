823d6e5360a13fe5790828f4574983643667dbc2
JayTSmith <justinsmth42@gmail.com>
2018-01-25 11:36:00 -0600

    Added a method for profiling code.

--- go-fish/main.py
+++ go-fish/main.py
@@ -24,6 +24,26 @@ def main():
     return fish
 
 
+def prof_main():
+    from cProfile import Profile
+
+    prof = Profile()
+    prof.enable()
+    fish = game.BasicGoFish(player_count=2)
+    fish.do_full_round()
+    prof.disable()
+
+    line_sep_char = '#'
+
+    win_string = '{1} Winners: {0}'.format(', '.join(['Player ' + p.name for p in fish.winner]),
+                                           line_sep_char)
+    sep = line_sep_char * len(win_string)
+    print('\n{0}\n{1}\n{0}'.format(sep, win_string))
+
+    prof.print_stats()
+    return fish
+
+
 if __name__ == '__main__':
     # Used if the script is used in an interactive environment
-    TEST_OBJ = main()
+    prof_main()
